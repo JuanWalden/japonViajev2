1f370adac3c3dd14fb4f4eba55e20c34f28bd433
JuanWalden <33595171+JuanWalden@users.noreply.github.com>
2025-08-21 14:21:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es" class="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planificador de Viaje a Japón 2025</title>
+    
+    <!-- PWA Manifest and Theme Color -->
+    <meta name="theme-color" content="#ef4444"/>
+    <link rel="manifest" href="./manifest.json">
+…        });
+    </script>
+</body>
+</html>
